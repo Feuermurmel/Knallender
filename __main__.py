14108acbeb2fc0985f5310bada7633a0394102e6
Michael Schwarz <michi.schwarz@gmail.com>
2016-01-11 03:18:36 +0100

    Compile in temporary directory.

--- __main__.py
+++ __main__.py
@@ -59,34 +59,41 @@ def datetime_from_iso_week(year, week):
 
 
 def main(start_year, start_week, weeks_per_page, pages, cell_size, paper_size):
-	out_path = '{:04}-W{:02}.asy'.format(start_year, start_week)
+	out_file_name = '{:04}-W{:02}'.format(start_year, start_week)
+	out_path = out_file_name + '.pdf'
 	first_week = datetime_from_iso_week(start_year, start_week)
 	
-	with open(out_path, 'w', encoding = 'utf-8') as file:
-		def write(line, *args):
-			print(line.format(*args), file = file)
+	with tempfile.TemporaryDirectory() as temp_dir:
+		asy_path = os.path.join(temp_dir, out_file_name + '.asy')
+		pdf_path = os.path.join(temp_dir, out_file_name + '.pdf')
 		
-		write('int weeks_per_page = {};', weeks_per_page)
-		write('pair paper_size = ({}mm, {}mm);', *paper_size)
-		write('pair cell_size = ({}mm, {}mm);', *cell_size)
-		write('pen row_header_font = font({!r}, {}mm);', 'OfficinaSansITC-Medium', 8)
-		write('pen cell_font = font({!r}, {}mm);', 'OfficinaSansITC-Book', 4)
-		write('pen cell_border_pen = {}pt + black;', 1)
-		write('real header_width = {}mm;', 15)
-		write('pair raster(real x, real y) {{ return (paper_size - (cell_size.x * 7 - header_width, cell_size.y * weeks_per_page)) / 2 + (cell_size.x * x, cell_size.y * y); }}')
+		with open(asy_path, 'w', encoding = 'utf-8') as file:
+			def write(line, *args):
+				print(line.format(*args), file = file)
+			
+			write('int weeks_per_page = {};', weeks_per_page)
+			write('pair paper_size = ({}mm, {}mm);', *paper_size)
+			write('pair cell_size = ({}mm, {}mm);', *cell_size)
+			write('pen row_header_font = font({!r}, {}mm);', 'OfficinaSansITC-Medium', 8)
+			write('pen cell_font = font({!r}, {}mm);', 'OfficinaSansITC-Book', 4)
+			write('pen cell_border_pen = {}pt + black;', 1)
+			write('real header_width = {}mm;', 15)
+			write('pair raster(real x, real y) {{ return (paper_size - (cell_size.x * 7 - header_width, cell_size.y * weeks_per_page)) / 2 + (cell_size.x * x, cell_size.y * y); }}')
+			
+			for i in range(1, weeks_per_page):
+				write('draw(raster(0, {0}) -- raster(7, {0}), cell_border_pen);', i)
+			
+			for i in range(1, 7):
+				write('draw(raster({0}, 0) -- raster({0}, weeks_per_page), cell_border_pen);', i)
+			
+			write('draw(box(raster(0, 0), raster(7, weeks_per_page)), cell_border_pen);')
+			
+			write('clip(box((0, 0), paper_size));')
+			write('fixedscaling((0, 0), paper_size);')
 		
-		for i in range(1, weeks_per_page):
-			write('draw(raster(0, {0}) -- raster(7, {0}), cell_border_pen);', i)
+		command('asy', '-f', 'pdf', '-tex', 'xelatex', '-o', pdf_path, asy_path)
 		
-		for i in range(1, 7):
-			write('draw(raster({0}, 0) -- raster({0}, weeks_per_page), cell_border_pen);', i)
-		
-		write('draw(box(raster(0, 0), raster(7, weeks_per_page)), cell_border_pen);')
-		
-		write('clip(box((0, 0), paper_size));')
-		write('fixedscaling((0, 0), paper_size);')
-	
-	command('asy', '-f', 'pdf', '-tex', 'xelatex', out_path)
+		os.rename(pdf_path, out_path)
 
 
 try:
