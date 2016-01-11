9308b606795b4a203909164f2f3379fe00cf9165
Michael Schwarz <michi.schwarz@gmail.com>
2016-01-11 17:12:02 +0100

    Output multiple pages.

--- __main__.py
+++ __main__.py
@@ -71,64 +71,65 @@ month_names = 'Januar Februar März April Mäi Juni Juli Auguscht Septämber Okt
 
 
 def main(start_year, start_week, weeks_per_page, pages, cell_size, paper_size):
-	out_file_name = '{:04}-W{:02}'.format(start_year, start_week)
-	out_path = out_file_name + '.pdf'
-	first_week = datetime_from_iso_week(start_year, start_week)
-	
-	with temporary_directory(True) as temp_dir:
-		asy_path = os.path.join(temp_dir, out_file_name + '.asy')
-		pdf_path = os.path.join(temp_dir, out_file_name + '.pdf')
+	for page in range(pages):
+		first_week = datetime_from_iso_week(start_year, start_week) + datetime.timedelta(weeks = page * weeks_per_page)
+		first_week_year, first_week_day, _ = first_week.isocalendar()
+		out_path = '{:04}-W{:02}.pdf'.format(first_week_year, first_week_day)
 		
-		with open(asy_path, 'w', encoding = 'utf-8') as file:
-			def write(line, *args):
-				print(line.format(*args), file = file)
+		with temporary_directory() as temp_dir:
+			asy_path = os.path.join(temp_dir, 'a.asy')
+			pdf_path = os.path.join(temp_dir, 'a.pdf')
 			
-			def label(font, size, alignment, text, position_expression, *args):
-				write('label("{{\\setmainfont{{{}}} {}}}", {}, {}, fontsize({}pt));', font, text, position_expression.format(*args), alignment, size)
-			
-			write('texpreamble("\\usepackage{{fontspec}}");')
-			write('int weeks_per_page = {};', weeks_per_page)
-			write('pair paper_size = ({}mm, {}mm);', *paper_size)
-			write('pair cell_size = ({}mm, {}mm);', *cell_size)
-			write('pen cell_border_pen = {}pt + black;', 1)
-			write('real header_width = {}mm;', 5)
-			write('pair raster(real x, real y) {{ return (paper_size - (cell_size.x * 7 - header_width, cell_size.y * weeks_per_page)) / 2 + (cell_size.x * x, cell_size.y * y); }}')
-			
-			for i in range(1, weeks_per_page):
-				write('draw(raster(0, {0}) -- raster(7, {0}), cell_border_pen);', i)
-			
-			for i in range(1, 7):
-				write('draw(raster({0}, 0) -- raster({0}, weeks_per_page), cell_border_pen);', i)
-			
-			for i in range(weeks_per_page):
-				week = first_week + datetime.timedelta(weeks = i)
-				_, week_number, _ = week.isocalendar()
+			with open(asy_path, 'w', encoding = 'utf-8') as file:
+				def write(line, *args):
+					print(line.format(*args), file = file)
 				
-				label('OfficinaSansITC-Medium', 24, 'W', '{}'.format(week_number), 'raster(0, {} + 1 / 2)', weeks_per_page - i - 1)
+				def label(font, size, alignment, text, position_expression, *args):
+					write('label("{{\\setmainfont{{{}}} {}}}", {}, {}, fontsize({}pt));', font, text, position_expression.format(*args), alignment, size)
 				
-				for j in range(7):
-					day = week + datetime.timedelta(j)
-					month_name = month_names[day.month]
+				write('texpreamble("\\usepackage{{fontspec}}");')
+				write('int weeks_per_page = {};', weeks_per_page)
+				write('pair paper_size = ({}mm, {}mm);', *paper_size)
+				write('pair cell_size = ({}mm, {}mm);', *cell_size)
+				write('pen cell_border_pen = {}pt + black;', 1)
+				write('real header_width = {}mm;', 5)
+				write('pair raster(real x, real y) {{ return (paper_size - (cell_size.x * 7 - header_width, cell_size.y * weeks_per_page)) / 2 + (cell_size.x * x, cell_size.y * y); }}')
+				
+				for i in range(1, weeks_per_page):
+					write('draw(raster(0, {0}) -- raster(7, {0}), cell_border_pen);', i)
+				
+				for i in range(1, 7):
+					write('draw(raster({0}, 0) -- raster({0}, weeks_per_page), cell_border_pen);', i)
+				
+				for i in range(weeks_per_page):
+					week = first_week + datetime.timedelta(weeks = i)
+					_, week_number, _ = week.isocalendar()
 					
-					if i == 0 and j == 0 or day.day == 1 and day.month == 1:
-						cell_label = '{}. {} {}'.format(day.day, month_name, day.year)
-					elif day.day == 1:
-						cell_label = '{}. {}'.format(day.day, month_name)
-					else:
-						cell_label = '{}'.format(day.day)
+					label('OfficinaSansITC-Medium', 24, 'W', '{}'.format(week_number), 'raster(0, {} + 1 / 2)', weeks_per_page - i - 1)
 					
-					label('OfficinaSansITC-Book', 12, 'SE', cell_label, 'raster({}, {})', j, weeks_per_page - i)
+					for j in range(7):
+						day = week + datetime.timedelta(j)
+						month_name = month_names[day.month - 1]
+						
+						if i == 0 and j == 0 or day.day == 1 and day.month == 1:
+							cell_label = '{}. {} {}'.format(day.day, month_name, day.year)
+						elif day.day == 1:
+							cell_label = '{}. {}'.format(day.day, month_name)
+						else:
+							cell_label = '{}'.format(day.day)
+						
+						label('OfficinaSansITC-Book', 12, 'SE', cell_label, 'raster({}, {})', j, weeks_per_page - i)
+				
+				write('draw(box(raster(0, 0), raster(7, weeks_per_page)), cell_border_pen);')
+				
+				write('clip(box((0, 0), paper_size));')
+				write('fixedscaling((0, 0), paper_size);')
 			
-			write('draw(box(raster(0, 0), raster(7, weeks_per_page)), cell_border_pen);')
+			command('asy', '-f', 'pdf', '-tex', 'xelatex', os.path.relpath(asy_path, temp_dir), cwd = temp_dir)
 			
-			write('clip(box((0, 0), paper_size));')
-			write('fixedscaling((0, 0), paper_size);')
-		
-		command('asy', '-f', 'pdf', '-tex', 'xelatex', os.path.relpath(asy_path, temp_dir), cwd = temp_dir)
-		
-		os.rename(pdf_path, out_path)
-		
-		log('Written {}.', out_path)
+			os.rename(pdf_path, out_path)
+			
+			log('Created {}.', out_path)
 
 
 try:
